870af01d4f43bc6a5f42019cd18160e7754e74ea
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-22 15:58:30 -0400

    complete first assignment: components

---
+++ first-angular-app/src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //selector: '[app-servers]',//by attribute
+  //selector: '.app-servers',//by class
+  template: '<app-server></app-server><app-server></app-server>',
+  styleUrl: './servers.component.css'
+})
+// export class ServersComponent implements OnInit { 
+//   constructor() {
+//     ngOnInit() {
+
+//     }
+//   }
+// }
+export class ServersComponent {}; 

026cbfae7b706baf9d095c52c509ce7705fcd7e0
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-22 16:39:08 -0400

    property binding and string interpolation

--- first-angular-app/src/app/servers/servers.component.ts
+++ first-angular-app/src/app/servers/servers.component.ts
@@ -4,14 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector: '[app-servers]',//by attribute
   //selector: '.app-servers',//by class
-  template: '<app-server></app-server><app-server></app-server>',
+  //template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
-// export class ServersComponent implements OnInit { 
-//   constructor() {
-//     ngOnInit() {
+export class ServersComponent implements OnInit { 
+  // property binding
+  //Property binding allows you to set values for properties or attributes of HTML elements dynamically
+  allowNewServer = false;
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000)
+  }
+    ngOnInit() {
 
-//     }
-//   }
-// }
-export class ServersComponent {}; 
+  }
+}
+

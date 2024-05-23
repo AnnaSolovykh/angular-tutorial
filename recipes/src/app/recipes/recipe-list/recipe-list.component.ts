733ce11f1adf9b1e95f3f6d0e6da1fb1f4b535ed
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-23 12:44:41 -0400

    add layout

---
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://plus.unsplash.com/premium_photo-1670895801174-8278045808f4?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ),
+    new Recipe('A Test Recipe TWO', 'This is simply a test TWO', 'https://plus.unsplash.com/premium_photo-1670895801174-8278045808f4?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' )
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}

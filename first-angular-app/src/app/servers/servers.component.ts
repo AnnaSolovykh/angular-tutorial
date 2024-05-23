95eff2569863bf06eb3c04ba8363281b2395a5a9
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-22 20:31:28 -0400

    add conditional styling

--- first-angular-app/src/app/servers/servers.component.ts
+++ first-angular-app/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'test server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(()=>{
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
     ngOnInit() {
   }
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus  = 'Server was created - its name is ' + this.serverName;
   }
   // onUpdateServerName(event: any) {

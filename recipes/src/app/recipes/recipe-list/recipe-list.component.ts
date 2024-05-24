2c6fe0e127678f20cc7c298f5e9aeb12e0e734a8
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-24 11:29:16 -0400

    add recipe details

--- recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] =[
     new Recipe('A Test Recipe', 'This is simply a test', 'https://plus.unsplash.com/premium_photo-1670895801174-8278045808f4?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' ),
     new Recipe('A Test Recipe TWO', 'This is simply a test TWO', 'https://plus.unsplash.com/premium_photo-1670895801174-8278045808f4?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' )
@@ -14,7 +15,9 @@ export class RecipeListComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
